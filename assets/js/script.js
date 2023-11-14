c61d774e326b1d5b301bb717f2b63403a019efbb
Benoit <fbenoit2077@gmail.com>
2023-11-13 21:29:05 -0500

    Event listener for search, make API req and data added to display when search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+const openWeatherApiKey = 'OPENWEATHER_API_KEY';
+const teleportApiKey = 'TELEPORT_API_KEY';
+
 document.addEventListener("DOMContentLoaded",() => {
     const cityInput = document.getElementById("cityInput");
     const searchButton = document.getElementById("searchButton");
@@ -6,18 +9,33 @@ document.addEventListener("DOMContentLoaded",() => {
     const forecast = document.getElementById("forecast");
 
 
-
-    fetch('https://api.teleport.org/api/')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
-  
     // Event listener for the search button
-    searchButton.addEventListener("click"), () => {
-        const cityName = cityInput.value;
-        // Make API request and update the HTML elements with the fetched data
-        // Openweather API and Teleport API here 
+searchButton.addEventListener("click", () => {
+  const cityName = cityInput.value;
+  
+  // Make API request to OpenWeather API
+  const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openWeatherApiKey}`;
+  fetch(openWeatherUrl)
+      .then(response => response.json())
+      .then(weatherData => {
+          // Process and display weatherData
+          // Update weatherInfo and weatherImage elements
+      })
+      .catch(error => console.error(error));
 
+  // Make API request to Teleport API
+  const teleportUrl = `https://api.teleport.org/api/city:${cityName}/`;
+  fetch(teleportUrl, {
+      headers: {
+          'Authorization': `Bearer ${teleportApiKey}`
+      }
+  })
+      .then(response => response.json())
+      .then(teleportData => {
+          // Process and display teleportData
+          // Update forecast and cityImage elements
+      })
+      .catch(error => console.error(error));
+});
 
-    };
 });
