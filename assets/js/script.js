43d6b5daec55fa779372ed2f0e348a6e2094927c
Benoit <fbenoit2077@gmail.com>
2023-11-15 21:48:34 -0500

    javascript changes to display the weather and organize forecast by date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,81 @@
-const openWeatherApiKey = 'c82895bdc50b848e2df6533322b114cb'; // Updated with your API key
-const teleportApiKey = 'TELEPORT_API_KEY'; // Replace with valid API key if needed
+const openWeatherApiKey = 'c82895bdc50b848e2df6533322b114cb'; // Replace with your valid API key
 
 document.addEventListener("DOMContentLoaded", () => {
-    const cityInput = document.getElementById("activityInput"); // Assuming this is the city input
+    const cityInput = document.getElementById("activityInput");
     const searchButton = document.getElementById("searchButton");
-
-    const weatherInfo = document.getElementById("weather"); // Display weather information here
-    const activityInfo = document.getElementById("activity"); // display activity info here
+    const currentWeatherDiv = document.getElementById("currentWeather");
+    const forecastDiv = document.getElementById("forecast");
+    const searchHistoryDiv = document.getElementById("searchHistory");
 
     searchButton.addEventListener("click", () => {
         const cityName = cityInput.value.trim();
         if (!cityName) {
-            alert("Please enter a city name."); // notification 
+            alert("Please enter a city name.");
             return;
         }
 
-        weatherInfo.textContent = "Loading weather data...";
         fetchWeather(cityName);
-        fetchTeleportData(cityName);
+        fetchForecast(cityName);
+        updateSearchHistory(cityName);
     });
 
     function fetchWeather(cityName) {
-        const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openWeatherApiKey}`;
-        fetch(openWeatherUrl)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`API request failed with status ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(weatherData => {
-                if (!weatherData || !weatherData.main || !weatherData.main.temp) {
-                    throw new Error("Invalid weather data format");
-                }
-                const temperatureFahrenheit = kelvinToFahrenheit(weatherData.main.temp);
-                weatherInfo.textContent = `Temperature: ${temperatureFahrenheit.toFixed(2)} °F`;
-                // Additional weather info updates...
-            })
-            .catch(error => {
-                weatherInfo.textContent = `Error: ${error.message}`;
-                console.error(error);
-            });
-    }
-
-    function fetchTeleportData(cityName) {
-        const teleportUrl = `https://api.teleport.org/api/cities/?search=${cityName}`;
-        fetch(teleportUrl)
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openWeatherApiKey}&units=imperial`;
+        fetch(weatherUrl)
             .then(response => response.json())
-            .then(teleportData => {
-                // Check if the city was found and data is available
-                if (teleportData && teleportData._embedded && teleportData._embedded['city:search-results'] && teleportData._embedded['city:search-results'].length > 0) {
-                    const cityData = teleportData._embedded['city:search-results'][0];
-                    const cityName = cityData.matching_full_name;
-                    const cityPopulation = cityData._links['city:item'].population;
-                    
-                    // Assuming you have an element with id 'cityInfo' to display the city's information
-                    const cityInfo = document.getElementById('cityInfo');
-                    cityInfo.textContent = `City: ${cityName}, Population: ${cityPopulation}`;
-                } else {
-                    throw new Error("City not found or data not available");
-                }
+            .then(data => {
+                displayCurrentWeather(data);
             })
-            .catch(error => console.error(error));
+            .catch(error => console.error("Error fetching current weather:", error));
     }
 
-    function kelvinToFahrenheit(kelvin) {
-        return (kelvin - 273.15) * 9/5 + 32;
+    function fetchForecast(cityName) {
+        const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${openWeatherApiKey}&units=imperial`;
+        fetch(forecastUrl)
+            .then(response => response.json())
+            .then(data => {
+                displayForecast(data);
+            })
+            .catch(error => console.error("Error fetching forecast:", error));
+    }
+
+    function displayCurrentWeather(data) {
+        currentWeatherDiv.innerHTML = `
+            <p>City: ${data.name}</p>
+            <p>Date: ${new Date().toLocaleDateString()}</p>
+            <p>Temperature: ${data.main.temp} °F</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Wind Speed: ${data.wind.speed} mph</p>
+        `;
+    }
+
+    function displayForecast(data) {
+        let forecasts = data.list;
+        let dailyForecasts = {};
+
+        // Organize forecasts by date
+        forecasts.forEach(forecast => {
+            let date = new Date(forecast.dt_txt).toLocaleDateString();
+            if (!dailyForecasts[date]) {
+                dailyForecasts[date] = [];
+            }
+            dailyForecasts[date].push(forecast);
+        });
+
+        // Calculate average temperature for each day and create display elements
+        forecastDiv.innerHTML = '';
+        Object.keys(dailyForecasts).forEach(date => {
+            let dayForecasts = dailyForecasts[date];
+            let avgTemp = dayForecasts.reduce((sum, forecast) => sum + forecast.main.temp, 0) / dayForecasts.length;
+
+            let forecastElement = document.createElement('div');
+            forecastElement.innerHTML = `<p>${date}: ${avgTemp.toFixed(2)} °F</p>`;
+            forecastDiv.appendChild(forecastElement);
+        });
+    }
+
+    function updateSearchHistory(cityName) {
+        // Add cityName to search history and update the display
+        searchHistoryDiv.innerHTML += `<p>${cityName}</p>`;
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
